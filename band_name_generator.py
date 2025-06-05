f5937fbc50c2fea32834f2787b991cc72d756b79
ifnidbi <atesturalca@gmail.com>
2025-06-05 14:03:47 -0400

    Deleted band_name_generator.py because I forgot to upload it inside of a folder

--- band_name_generator.py
+++
@@ -1,5 +0,0 @@
-print('Welcome to my Band Name Generator!')
-city_name = input('Which city did you grow up in?.\n')
-pet_name = input('What is your pet\'s name?\n')
-
-print(f'Your band name can be {city_name} {pet_name}!')
